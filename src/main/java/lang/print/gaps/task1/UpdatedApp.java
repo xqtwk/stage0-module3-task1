0cab6d7c4d929f244d6733cba35fb7d40d0d4307
Richard <xqtwkk@gmail.com>
2022-07-18 01:08:30 +0300

    must be done

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.print("I'm updated" + "\n");
     }
 }
