650d13c107af8a034e677c7801253545e7cfb48c
Richard <xqtwkk@gmail.com>
2022-07-18 01:20:39 +0300

    attempt to fix issues n2?

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.print("I'm updated" + "\n" + "+");
+        System.out.print("I'm updated" + "\n");
     }
 }
